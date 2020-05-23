b6e9024416cf487a5cf8efb51a5670ef9b65b9a4
lajawanti <lajawanti.dhake@gmail.com>
2020-05-22 23:52:08 -0400

    searchCountry functionality added

--- JS/map.js
+++ JS/map.js
@@ -90,3 +90,62 @@ function createMarker(latlng, country_code, name, confirmed, recovered, dead, up
     });
 }
 
+/****************** searchCountry() *****************/
+function searchCountry(country) {
+    var countryNameToSearch = document.getElementById('country-name-input').value.toLowerCase();
+    
+    foundCountry = country_Data.filter(function(country, index) {
+            return countryNameToSearch === country.location.toLowerCase();
+    });
+    
+    if(foundCountry.length === 0) {
+        foundCountry = country_Data.filter(function(country, index) {
+            return countryNameToSearch.substring(0,1) === country.location.substring(0,1).toLowerCase();
+        });
+    }
+    console.log("FOUND country  : ", foundCountry);
+    
+    displayCountries(foundCountry); 
+//    showMarkers(foundCountry); // will show markers on map for found satores only
+    setOnClickListener(); // onclick in store-list display area will pop up marker info on map
+}
+
+// ************ DISPLAY STORES *************
+function displayCountries(foundCountry) {
+    var countryHtml = '';
+    foundCountry.forEach(function(country, index){ 
+        var countryName = country.location;
+        var country_code = country.country_code.toUpperCase();
+        var confirmed = country.confirmed;
+        var recovered = country.recovered;
+        var dead = country.dead;
+        var updatedOn = country.updated.substring(0, 10);
+
+        countryHtml += ` 
+            <div class = 'country-container'>
+                <div class = 'country-container-background' >
+                    <div class = "country-info-container">
+                        <div class = 'country-name'>
+                            <span> ${countryName} </span>
+                        </div>
+                        <div class = 'country-other-info'>
+                            <span> Confirmed : ${confirmed} </span><br/>
+                            <span> Recovered : ${recovered} </span><br/>
+                            <span> Death : ${dead} </span>
+                        </div>
+                        <div class = 'store-phone-number'> 
+                             Updated-on : ${updatedOn}
+                        </div>
+                    </div>
+                    <div class = "store-number-container">
+                        <div class = 'store-number'>
+                            ${country_code}
+                        </div>
+                    </div>    
+                </div>
+            </div>
+         `
+    });
+    document.querySelector('.country-list-container').style.display = 'flex';
+    document.querySelector('.country-list').innerHTML = countryHtml; //select element with class "stores-list" and display storeHtml there.
+}
